22d00bf4217a7cb49493ed11c8b409942b7150e8
Eight8 <66164881+Eight8-7020@users.noreply.github.com>
2020-10-29 13:57:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-module.exports = function({ webserver }) {
+module.exports = function({ webserver, commands, bot, config }) {
   webserver.get("/", (req, res) => {
     res.sendFile("home.html", {
       root: "./"
     });
+  });
+  commands.addGlobalCommand('home', '') async (msg, args, thread) => {
+    bot.createMessage(msg.channel.id, config.url)
   });
 };
